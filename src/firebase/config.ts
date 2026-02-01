4034c50ff595f4a045327d3b7ed4f098594d75be
Christian Aduku <adukuchristian11@gmail.com>
2026-02-01 22:30:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2036145567-57177",
+  "appId": "1:524864712766:web:11633ac6ada04034c2216f",
+  "apiKey": "AIzaSyC0G8H3IDPzg4olH1xFsh3TTxw5T6mdIsw",
+  "authDomain": "studio-2036145567-57177.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "524864712766"
+};
